9975871c0634b4edf917a0842df19e7d1fa8780a
Johannes Pietrzyk <johannes.pietrzyk@tu-dresden.de>
2025-02-25 16:42:01 +0100

    .

--- scripts/setup_tsl.sh
+++ scripts/setup_tsl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo mkdir -p /usr/include/tsl
-sudo curl -L -s "https://github.com/db-tu-dresden/TSL/releases/latest/download/setup_tsl.sh" | /bin/bash -s -- --system
+mkdir -p /usr/include/tsl
+curl -L -s "https://github.com/db-tu-dresden/TSL/releases/latest/download/setup_tsl.sh" | /bin/bash -s -- --system
